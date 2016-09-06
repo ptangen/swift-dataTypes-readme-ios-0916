36c6567aa8e51c35172d4071429794ca225cfbb2
Paul Tangen <ptangen@ptangen.com>
2016-09-06 09:13:56 -0400

    Complete the data types lesson.

---
+++ dataTypes.playground/Contents.swift
@@ -0,0 +1,22 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+let thirdPlanet = "earth"
+
+let fifthPlanet: String = "Jupiter"
+// fifthPlanet is a constant of type string
+
+let sixthPlanet: String = "Saturn"
+// sixthPlanet is a constant of type string
+
+let seventhPlanet = "Uranus"
+// seventhPlanet is a constant of type string
+
+let numberOfCountriesInAfrica = 54
+
+let numberOfMoonsOfSaturn = 62
+
+print("There are \(numberOfMoonsOfSaturn) moons orbiting \(sixthPlanet).")
